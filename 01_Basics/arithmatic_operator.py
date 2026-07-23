dad2d51a40b0cf2354f117c6a1453d3b4e8e3e13
Yash Kumar <yk165218@gmail.com>
2026-07-23 09:46:42 +0530

    Create arithmatic_operator.py

---
+++ 01_Basics/arithmatic_operator.py
@@ -0,0 +1,37 @@
+# arithmetic operators
+# + , - , *, /, //, **, %
+
+b = 12
+a = 36
+print (a + b)
+print (b - a)
+# it always follow bodmas step
+# in python, anything we write in the form of p / q, its answer will be in float
+# for example 12 / 2 != 6 but 12 /2 = 6.0
+
+# //  is floor  divison which return only an integer value
+print(a / b) # answer will be 3.0
+print (a // b) # answer will be 3 only i.e int value
+
+
+# ** denotes exponential 
+print( 5 ** 2)
+print (5 ** 3)
+
+# % operator is used ot find the remainder 
+
+print(a % b)
+
+# arithematic operator worls only for number
+
+# assignment operator
+
+# for string we use concatenation and multiplication
+
+x = " hello"
+y = " I LOVE AA"
+print (x + y )
+
+# multiplication helps us to repeat the things the no of times
+
+print ( (x + y ) * 3)
