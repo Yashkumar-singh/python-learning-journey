46b798ef3fc0c81f023ab11a4d656609745bc70d
Yash Kumar <yk165218@gmail.com>
2026-07-23 09:47:37 +0530

    Create comparison_operator.py

---
+++ 01_Basics/comparison_operator.py
@@ -0,0 +1,21 @@
+# comparison  operator
+'''
+a = 12
+b = 45
+
+print(a > b)
+
+# ==  !=  >  <  >=  <=
+print ( 12 != 12.0)
+print ( (12 == 12) == False)
+print ( (12 == 12) != False)
+
+# logical operator
+# and , or , not ---- operator
+print(not 12 == 12 and False)
+'''
+
+print ( 125 > 130)
+print ((456 == 456) != (235 == 236))
+print(12 < 10 or 45 == 56 or 69 > 70 or 15 != 3)
+print (True and bool (0))
